deffcde1fa3a3cd4b3277463d8f4bd99a15919b5
ColPaulR <35244547+ColPaulR@users.noreply.github.com>
2024-08-07 14:37:43 -0400

    wip

--- Telnet.cpp
+++ Telnet.cpp
@@ -2,12 +2,14 @@
 //
 #define WIN32_LEAN_AND_MEAN
 
-#include "GrblParser/GrblParserC.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+
+#include "GrblParser/GrblParserC.h"
+#include "SerialDebug.h"
 
 #define DEFAULT_BUFLEN 512
 
@@ -87,18 +89,21 @@ int TelnetSend(SOCKET ConnectSocket, char *szSend) {
 }
 
 void TelnetTask(SOCKET ConnectSocket) {
-  char recvbuf[DEFAULT_BUFLEN];
+  char recvbuf[DEFAULT_BUFLEN+1];
   int iResult;
   int recvbuflen = DEFAULT_BUFLEN;
 
   iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
   if (iResult > 0) {
-    // Ensure buffer is null terminated to handle as string
-    recvbuf[iResult] = 0;
     // Send 1 character at a time to GRBLParser.
     for (int iLooper = 0; iLooper < iResult; iLooper++) {
       collect(recvbuf[iLooper]);
     }
+    #if (GRBL_STATUS_ECHO)
+      // Ensure buffer is null terminated to handle as string
+      recvbuf[iResult] = 0;
+      puts(recvbuf);
+    #endif
     // printf("Bytes received: %d\n\t%s\n", iResult, recvbuf);
   } else if (iResult == 0)
     printf("Connection closed\n");
